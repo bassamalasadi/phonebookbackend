78b9b9bd14cb88eddc9a9abde69843febc326324
bassamalasadi <bassamalasadi@gmail.com>
2019-08-03 19:02:25 +0300

    uibuild

---
+++ build/precache-manifest.9fb2d32455f7fcff63884a18622465cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3ba8ad69f172ddee87e41b86727b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b86b23f59594ba9328",
+    "url": "/static/css/main.ea33e1a6.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "74b86b23f59594ba9328",
+    "url": "/static/js/main.29e91fce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,116 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const Person = require('./models/persons')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(cors())
+
+app.get('/api/notes', (req, res) => {
+  Person.find({}).then(people => {
+    res.json(people)
+  })
+  .catch(console.log('error'))
+})
+
+
+morgan.token('data',(req, res) => {
+  if (req.method === 'POST') {
+    return (
+      JSON.stringify({
+          name: req.body.name,
+          number: req.body.number,
+      })
+  )}else{
+    return ' '
+}})
+
+app.use(morgan(':method :url :status :res[data-length] - :response-time ms :data'))
+
+
+
+
+app.post('/api/persons',(req,res,next) => {
+  const body = req.body
+  const person = new Person({
+    name : body.name,
+    number: body.number,
+    
+  })
+  person.save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      res.json(savedAndFormattedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id',(req, res, next)=> {
+ Person.findById(req.params.id).then(person => {
+  if (person) {
+    res.json(person.toJSON())
+} else {
+    res.status(404).end()
+}
+})
+.catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+  })
+app.get('/info', (req, res) => {
+  const num = persons.length
+  const infoPage = `<p>Phonebook has info for ${num} people.</p> <p>${new Date}</p>`
+  res.send(infoPage)
+})
+
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => {
+      res.json(persons.map(person => person.toJSON()))
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+      name: body.name,
+      number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+      .then(updatedPerson => {
+          res.json(updatedPerson.toJSON())
+      })
+      .catch(error => next(error))
+      
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
